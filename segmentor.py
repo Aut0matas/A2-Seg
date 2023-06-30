2628285c7ed8264632064349ebceb8940f064d22
Aut0matas <43371529+Aut0matas@users.noreply.github.com>
2023-06-30 18:44:53 +0800

    update segmentor

--- segmentor.py
+++ segmentor.py
@@ -31,10 +31,14 @@ class up_conv(nn.Module):
             nn.ReLU(inplace=True),
         )
 
+    def forward(self, x):
+        x = self.up(x)
+        return x
 
-class Attention_block(nn.Module):
+
+class AttentionGate(nn.Module):
     def __init__(self, F_g, F_l, F_int):
-        super(Attention_block, self).__init__()
+        super(AttentionGate, self).__init__()
         self.W_g = nn.Sequential(
             nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
             nn.BatchNorm2d(F_int),
@@ -62,71 +66,6 @@ class Attention_block(nn.Module):
         return x * psi
 
 
-class U_Net(nn.Module):
-    def __init__(self, img_ch=3, output_ch=1):
-        super(U_Net, self).__init__()
-
-        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.Conv1 = DoubleConv(ch_in=img_ch, ch_out=64)
-        self.Conv2 = DoubleConv(ch_in=64, ch_out=128)
-        self.Conv3 = DoubleConv(ch_in=128, ch_out=256)
-        self.Conv4 = DoubleConv(ch_in=256, ch_out=512)
-        self.Conv5 = DoubleConv(ch_in=512, ch_out=1024)
-
-        self.Up5 = up_conv(ch_in=1024, ch_out=512)
-        self.Up_conv5 = DoubleConv(ch_in=1024, ch_out=512)
-
-        self.Up4 = up_conv(ch_in=512, ch_out=256)
-        self.Up_conv4 = DoubleConv(ch_in=512, ch_out=256)
-
-        self.Up3 = up_conv(ch_in=256, ch_out=128)
-        self.Up_conv3 = DoubleConv(ch_in=256, ch_out=128)
-
-        self.Up2 = up_conv(ch_in=128, ch_out=64)
-        self.Up_conv2 = DoubleConv(ch_in=128, ch_out=64)
-
-        self.Conv_1x1 = nn.Conv2d(64, output_ch, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-        # encoding path
-        x1 = self.Conv1(x)
-
-        x2 = self.Maxpool(x1)
-        x2 = self.Conv2(x2)
-
-        x3 = self.Maxpool(x2)
-        x3 = self.Conv3(x3)
-
-        x4 = self.Maxpool(x3)
-        x4 = self.Conv4(x4)
-
-        x5 = self.Maxpool(x4)
-        x5 = self.Conv5(x5)
-
-        # decoding + concat path
-        d5 = self.Up5(x5)
-        d5 = torch.cat((x4, d5), dim=1)
-
-        d5 = self.Up_conv5(d5)
-
-        d4 = self.Up4(d5)
-        d4 = torch.cat((x3, d4), dim=1)
-        d4 = self.Up_conv4(d4)
-
-        d3 = self.Up3(d4)
-        d3 = torch.cat((x2, d3), dim=1)
-        d3 = self.Up_conv3(d3)
-
-        d2 = self.Up2(d3)
-        d2 = torch.cat((x1, d2), dim=1)
-        d2 = self.Up_conv2(d2)
-
-        d1 = self.Conv_1x1(d2)
-
-        return d1
-
-
 class NetS(nn.Module):
     def __init__(self, img_ch=3, output_ch=1):
         super(NetS, self).__init__()
@@ -139,19 +78,19 @@ class NetS(nn.Module):
         self.Conv5 = DoubleConv(ch_in=512, ch_out=1024)
 
         self.Up5 = up_conv(ch_in=1024, ch_out=512)
-        self.Att5 = Attention_block(F_g=512, F_l=512, F_int=256)
+        self.Att5 = AttentionGate(F_g=512, F_l=512, F_int=256)
         self.Up_conv5 = DoubleConv(ch_in=1024, ch_out=512)
 
         self.Up4 = up_conv(ch_in=512, ch_out=256)
-        self.Att4 = Attention_block(F_g=256, F_l=256, F_int=128)
+        self.Att4 = AttentionGate(F_g=256, F_l=256, F_int=128)
         self.Up_conv4 = DoubleConv(ch_in=512, ch_out=256)
 
         self.Up3 = up_conv(ch_in=256, ch_out=128)
-        self.Att3 = Attention_block(F_g=128, F_l=128, F_int=64)
+        self.Att3 = AttentionGate(F_g=128, F_l=128, F_int=64)
         self.Up_conv3 = DoubleConv(ch_in=256, ch_out=128)
 
         self.Up2 = up_conv(ch_in=128, ch_out=64)
-        self.Att2 = Attention_block(F_g=64, F_l=64, F_int=32)
+        self.Att2 = AttentionGate(F_g=64, F_l=64, F_int=32)
         self.Up_conv2 = DoubleConv(ch_in=128, ch_out=64)
 
         self.Conv_1x1 = nn.Conv2d(64, output_ch, kernel_size=1, stride=1, padding=0)
