20e384b9e30769b8541f6aaa7f41ac7862f28bf9
Aut0matas <43371529+Aut0matas@users.noreply.github.com>
2023-07-01 01:39:51 +0800

    add channel attention block

--- critic.py
+++ critic.py
@@ -22,6 +22,24 @@ class ConvBlock(nn.Module):
         return x
 
 
+# Channel-wise Attention Block
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1 = nn.Conv2d(in_channels, in_channels // reduction_ratio, 1, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Conv2d(in_channels // reduction_ratio, in_channels, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
 class NetC(nn.Module):
     def __init__(self):
         super(NetC, self).__init__()
@@ -63,4 +81,5 @@ class NetC(nn.Module):
             ),
             1,
         )
-        return output
+        output_att = ChannelAttention(output) * output
+        return output_att
